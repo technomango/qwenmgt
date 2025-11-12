a52c05b5041f1cd65031d5591ac48a54a9e5ea16
Prithiv Sakthi <prithivMLmods@users.noreply.huggingface.co>
2025-11-12 15:35:49 +0000

    update app

--- app.py
+++ app.py
@@ -235,7 +235,7 @@ with gr.Blocks(css=css, theme=steel_blue_theme) as demo:
             inputs=[input_image, prompt, lora_adapter],
             outputs=[output_image, seed],
             fn=infer_example,
-            cache_examples="lazy",
+            cache_examples=False,
             label="Examples"
         )
 
