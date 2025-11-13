fcd71eaeb389326cf9641e1e37974fb78ccb4762
Prithiv Sakthi <prithivMLmods@users.noreply.huggingface.co>
2025-11-13 08:01:47 +0000

    update app

--- app.py
+++ app.py
@@ -225,7 +225,7 @@ with gr.Blocks(css=css, theme=steel_blue_theme) as demo:
                     placeholder="e.g., transform into anime..",
                 )
 
-                run_button = gr.Button("Run", variant="primary")
+                run_button = gr.Button("Run", variant="primary", scale=0)
 
             with gr.Column():
                 output_image = gr.Image(label="Output Image", interactive=False, format="png", height=290)
@@ -236,7 +236,7 @@ with gr.Blocks(css=css, theme=steel_blue_theme) as demo:
                         choices=["Photo-to-Anime", "Multiple-Angles", "Light-Restoration", "Relight"],
                         value="Photo-to-Anime"
                     )
-                with gr.Accordion("⚙️ Advanced Settings", open=False):
+                with gr.Accordion("Advanced Settings", open=False):
                     seed = gr.Slider(label="Seed", minimum=0, maximum=MAX_SEED, step=1, value=0)
                     randomize_seed = gr.Checkbox(label="Randomize Seed", value=True)
                     guidance_scale = gr.Slider(label="Guidance Scale", minimum=1.0, maximum=10.0, step=0.1, value=1.0)
